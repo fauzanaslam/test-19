03ed082b596436c41ca7eb6d0239f5f551c5fec9
fauzanaslam <fauzanaslamdhani@gmail.com>
2025-05-02 15:17:01 +0700

    test .bat

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
-docker build -f dockerfile -t angular/test-19:1.0.1 . && docker compose up -d --force-recreate
+echo Mulai install dependencies
+npm install
+
+echo Mulai build aplikasi Angular
+npm run build --configuration production
+
+echo Build Docker image
+docker build -f Dockerfile -t angular/test-19:1.0.1 .
+
+echo Jalankan ulang container dengan Docker Compose
+docker-compose up -d --force-recreate
